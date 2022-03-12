302df445d988bc50f222e845c9f551d01edb8a16
hugosenari <hugosenari@gmail.com>
2022-03-12 03:29:54 -0300

    chore: re index tencil

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Tencil is a mustache-compatible JSON based template engine for Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."tencil-main".url = "path:./main";
+  inputs."tencil-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tencil-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ main/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Tencil is a mustache-compatible JSON based template engine for Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-tencil-main.flake = false;
+  inputs.src-tencil-main.owner = "enimatek-nl";
+  inputs.src-tencil-main.ref   = "refs/heads/main";
+  inputs.src-tencil-main.repo  = "tencil";
+  inputs.src-tencil-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-tencil-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-tencil-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
